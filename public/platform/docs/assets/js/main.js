47750d271d889bf6bd81942027553befd6dc1972
andreimititelu <andreimititelu@outlook.com>
2017-11-05 11:57:38 +0200

    UI Changes

---
+++ public/platform/docs/assets/js/main.js
@@ -0,0 +1,49 @@
+console.log ("Inside Main!");
+
+var selection = {};
+selection.devices = [];
+
+$(document).ready(function () {
+
+  $("#search").click (function () {
+    $("[type='checkbox']:checked").each(function () {
+      //console.log (this.parentNode.parentNode);
+      var parent = this.parentNode.parentNode;
+      // console.log (parseInt($("select", parent).val().replace(/\D/g, '')));
+      var device = {};
+
+      device.name = this.className;
+      device.duration = parseInt($("select", parent).val().replace(/\D/g, ''));
+
+      selection.devices.push(device);
+      // console.log (this.className);
+    });
+
+    console.log (selection);
+
+
+    /* $.ajax({
+      url: "http://localhost:3000/api/search/",
+      beforeSend: function( xhr ) {
+        xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+      }
+    })
+      .done(function( data ) {
+        if ( console && console.log ) {
+          console.log( "Sample of data:", data.slice( 0, 100 ) );
+        }
+      });
+    }); */
+
+    // Get URL
+    var url = window.location.href;
+    var arr = url.split("/");
+    var hostname = arr[0] + "//" + arr[2];
+
+    // Search
+    $.post( hostname + "/api/search/", selection)
+      .done(function( data ) {
+        console.log( "Data Loaded: ", data );
+      });
+  });
+});
